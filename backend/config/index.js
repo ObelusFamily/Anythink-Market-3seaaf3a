580d8c1140828b1cf7dafc0815da63e32226c3cc
Deanna M. Presswood <deanna.presswood@ourbranch.com>
2023-05-09 13:03:09 +0000

    Updates backend api image files

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,3 +1,2 @@
-module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
-};
+export const secret = process.env.NODE_ENV === 'production' ? process.env.SECRET : 'OPENAI_API_KEY';
+
